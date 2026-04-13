5d5594914cc5019eff55a4525c0d50e6ebfbf241
AasthaOswal <aasthaoswal29@gmail.com>
2026-04-13 08:53:40 +0530

    Changed date function in UTC

--- backend/utils/goals/getDateRange.js
+++ backend/utils/goals/getDateRange.js
@@ -1,27 +1,54 @@
+// Dates in UTC because mongo stored them in UTC, so while comparing also we will have to do UTC
+
 export const getDateRange = (period) => {
     const now = new Date();
+
     let start, end;
 
     if (period === "daily") {
-        start = new Date(now.setHours(0, 0, 0, 0));
-        end = new Date();
+        start = new Date(Date.UTC(
+            now.getUTCFullYear(),
+            now.getUTCMonth(),
+            now.getUTCDate(),
+            0, 0, 0
+        ));
+        end = now;
     }
 
     if (period === "weekly") {
-        const day = now.getDay();
-        start = new Date(now.setDate(now.getDate() - day));
-        start.setHours(0, 0, 0, 0);
-        end = new Date();
+        const day = now.getUTCDay();
+
+        const startDate = new Date(now);
+        startDate.setUTCDate(now.getUTCDate() - day);
+
+        start = new Date(Date.UTC(
+            startDate.getUTCFullYear(),
+            startDate.getUTCMonth(),
+            startDate.getUTCDate(),
+            0, 0, 0
+        ));
+
+        end = now;
     }
 
     if (period === "monthly") {
-        start = new Date(now.getFullYear(), now.getMonth(), 1);
-        end = new Date();
+        start = new Date(Date.UTC(
+            now.getUTCFullYear(),
+            now.getUTCMonth(),
+            1,
+            0, 0, 0
+        ));
+        end = now;
     }
 
     if (period === "yearly") {
-        start = new Date(now.getFullYear(), 0, 1);
-        end = new Date();
+        start = new Date(Date.UTC(
+            now.getUTCFullYear(),
+            0,
+            1,
+            0, 0, 0
+        ));
+        end = now;
     }
 
     return { start, end };
